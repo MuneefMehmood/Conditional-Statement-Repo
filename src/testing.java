326fec4f651da2eb9da3cbbbbf435012ff147c4c
Muneef <rai.muneef@yahoo.com>
2019-08-02 01:04:15 -0400

    initial commit

---
+++ src/testing.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20;
+		int b = 20;
+		
+		boolean ConditionCheck = a==b;
+		
+		if(ConditionCheck) {
+			System.out.println("Hello Student");
+			
+		}else { 
+		
+		System.out.println("No Way");
+		}
+		
+		//else if
+		//<,>,<=,>=
+		//!
+		
+		if (a==b) {
+
+			System.out.println("if you work hard you will succeeed");
+			
+			
+		}  {if (b==b) {
+			System.out.println("Your Parents Will Proud Of You");
+		}   
+		
+			
+			
+			Scanner YourGrade = new Scanner(System.in);
+			System.out.println("Please Enter Your Grade");
+			int Number = YourGrade.nextInt();
+			
+			if (Number< 40) {
+				System.out.println("Fail, Go Study and Get Good Grades");
+				System.out.println("Make Your Parents Proud!");
+				
+				
+			}
+			
+
+		
+			
+			
+		}
+
+		
+		
+		
+		
+		
+	}
+
+
+
+
+	}
+
+
